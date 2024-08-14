fbfecc289af5cfe152c560804f7c3ea391367e18
GTM35 <035guuh@gmail.com>
2024-08-14 10:12:57 -0300

    init: setup redux

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 
-export const store = configureStore({ reducer: {}, middleware: () => logger });
+export const store = configureStore({
+  reducer: {},
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+});
 
 export type RootReducer = ReturnType<typeof store.getState>;
